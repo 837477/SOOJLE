53f82b871e9835916d1d65130daba9f202d603ac
837477 <8374770@gmail.com>
2019-11-29 20:56:58 +0900

    SOOJLE update

--- apps/newsfeed.py
+++ apps/newsfeed.py
@@ -8,7 +8,6 @@ from numpy import dot
 from numpy.linalg import norm
 import numpy
 import operator
-import time
 #####################################
 from db_management import *
 from global_func import *
@@ -73,12 +72,9 @@ def get_popularity_newsfeed():
 @BP.route('/get_recommendation_newsfeed')
 @jwt_optional
 def get_recommendation_newsfeed():
-	##########################################################	
-	POST_LIST_time = time.time()
 	POST_LIST = find_all_posts(g.db, _id=1, topic=1, ft_vector=1, fav_cnt=1, view=1, tag=1, title=1, url=1, img=1, date=1, limit_=15000)
 
 	POST_LIST = list(POST_LIST)
-	print("DB에서 최신정렬 상위 15000개 POST를 불러온 시간 :", time.time() - POST_LIST_time)
 
 	if get_jwt_identity():
 		#logging
@@ -89,23 +85,15 @@ def get_recommendation_newsfeed():
 
 		if USER is None: abort(400)
 
-		##########################################################
-		MAX_time = time.time()
 		#캐싱된 가장 높은 좋아요 수를 가져온다.
 		Maxfav_cnt = find_variable(g.db, 'highest_fav_cnt')
 		#캐싱된 가장 높은 조회수를 가져온다.
 		Maxviews = find_variable(g.db, 'highest_view_cnt')
-		print("DB에 캐싱된 최대 좋아요/조회수를 불러온 시간 :", time.time() - MAX_time)
 
-		###############################################################
-		SIMILARITY_time = time.time()
 		for POST in POST_LIST:
-			TOS_time = time.time()
 			#TOS 작업
 			TOS = dot(USER['topic'], POST['topic'])/(norm(USER['topic'])*norm(POST['topic']))
-			print("TOS 분석 시간 :", time.time() - TOS_time)
 
-			TAS_time = time.time()
 			#TAS 작업
 			USER_TAG = USER['tag'].keys()
 			TAG = USER_TAG & set(POST['tag'])
@@ -113,24 +101,17 @@ def get_recommendation_newsfeed():
 			for i in TAG:
 				inter_sum += USER['tag'][i]
 			TAS = inter_sum / USER['tag_sum']
-			print("TAS 분석 시간 :", time.time() - TAS_time)
 			
-			FAS_time = time.time()
 			#FAS 작업
 			FAS = FastText.vec_sim(USER['ft_vector'], POST['ft_vector'])
-			print("FAS 분석 시간 :", time.time() - FAS_time)
 
-			IS_time = time.time()
 			#IS 작업
 			IS = (((POST['fav_cnt']/Maxfav_cnt)*3) + ((POST['view']/Maxviews)))
 			IS /= 4
-			print("IS 분석 시간 :", time.time() - IS_time)
 
-			RAND_time = time.time()
 			#RANDOM 작업
 			RANDOM = numpy.random.random()
 			RANDOM *= 2
-			print("RANDOM값 추출 시간 :", time.time() - RAND_time)
 
 			#최종 값 저장
 			result = TOS + TAS + FAS + RANDOM
@@ -142,13 +123,8 @@ def get_recommendation_newsfeed():
 
 			POST['similarity'] = result
 
-		print("15000개의 POST와 한 명의 사용자간의 유사도 분석 시간 :", time.time() - SIMILARITY_time)
-
-		#########################################################################
-		SORT_time = time.time()
 		#similarity를 기준으로 내림차순 정렬.
 		POST_LIST = sorted(POST_LIST, key=operator.itemgetter('similarity'), reverse=True)
-		print("15000개 POST similarity 정렬 시간 :", time.time() - SORT_time)
 	
 	#Token이 안들어왔을 때
 	else:
