19f576be62fcaf7db25cbe56eb1f91933cc62062
서정민 <837477>
2020-02-15 19:50:41 +0900

    soojle_update

--- apps/newsfeed.py
+++ apps/newsfeed.py
@@ -37,7 +37,7 @@ def get_newsfeed_of_topic(newsfeed_name):
 	#end_date 처리!
 	for POST in POST_LIST:
 		#포스트에 end_date가 존재하고, 현재 날짜가 더 커버리면 (이미 지난 글)
-		if ('end_date' in POST) and (now_date > POST['end_date']):
+		if (('end_date' in POST) and (now_date > POST['end_date'])) or (get_plus_day(30) > POST['date']):
 			#해당 포스트 삭제.
 			POST_LIST.remove(POST)
 
