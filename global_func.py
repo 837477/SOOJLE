8a02a27bb7db026c50de5ae40e5f0e2e59595c5b
서정민 <837477>
2020-03-08 21:48:57 +0900

    soojle update

--- global_func.py
+++ global_func.py
@@ -179,7 +179,7 @@ def real_time_insert():
 			if len(realtime_result) == SJ_REALTIME_LIMIT:
 				break
 			
-	insert_search_realtime(db, real_time_result)
+	insert_search_realtime(db, realtime_result)
 	
 	if db_client is not None:
 		db_client.close()
