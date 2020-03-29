1712bcdd933d8bc78e5a20a1a73c12e439fcf5ff
NB <altmshfkgudtjr@naver.com>
2020-03-30 00:38:51 +0900

    sejong guide layout changed

--- static/js/sejongGuide.js
+++ static/js/sejongGuide.js
@@ -28,21 +28,22 @@ function Insert_Sejong_Guide() {
 	let div = 	`
 					<div class="guide_title noselect">앱 설치하기</div>
 					<div class="guide_subtitle noselect">
-						세종대학교 통합 모바일 앱과 UCheck 앱을 설치할 수 있습니다!
+						세종대학교 필수 어플! 통합 모바일 앱과 UCheck 앱을 설치할 수 있습니다.
 					</div>
 					<div class="guide_body guide_body_first noselect">
 						<div class="guide_app_mobile_cont">
 							<img src="static/image/app_guide.png" class="guide_app_mobile wow animated fadeInLeft">
-						</div><div id="guide_app_container" class="guide_app_container">
-							<a title="App Store" target="_blank" href="https://apps.apple.com/kr/app/%EC%84%B8%EC%A2%85%EB%8C%80%ED%95%99%EA%B5%90-%ED%86%B5%ED%95%A9-%EB%AA%A8%EB%B0%94%EC%9D%BC-%EC%95%B1/id1465703023?mt=8" style="overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2019-08-29&kind=iossoftware&bubble=apple_music) no-repeat center;" class="guide_app_link_btn_apple"></a>
-							<a title="Play Store" target="_blank" class="sejongapp_btn" href='https://play.google.com/store/apps/details?id=kr.ac.sejong.smartcampus&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' class="guide_app_link_btn_google"></a>
-						</div>
-						<div style="position: relative; width:100%; height:50px;"></div>
-						<div class="guide_app_mobile_cont">
-							<img src="static/image/ucheck_guide.png" class="guide_app_mobile wow animated fadeInLeft">
-						</div><div id="guide_app_container" class="guide_app_container">
-							<a title="App Store" target="_blank" href="https://apps.apple.com/us/app/ucheck-plus/id1139582817?mt=8" style="overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2019-08-29&kind=iossoftware&bubble=apple_music) no-repeat center;" class="guide_app_link_btn_apple"></a>
-							<a title="Play Store" target="_blank" class="sejongapp_btn" href='https://play.google.com/store/apps/details?id=com.libeka.attendance.ucheckplusstud'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' class="guide_app_link_btn_google"></a>
+							<div id="guide_app_container" class="guide_app_container">
+								<a title="App Store" target="_blank" href="https://apps.apple.com/kr/app/%EC%84%B8%EC%A2%85%EB%8C%80%ED%95%99%EA%B5%90-%ED%86%B5%ED%95%A9-%EB%AA%A8%EB%B0%94%EC%9D%BC-%EC%95%B1/id1465703023?mt=8" style="overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2019-08-29&kind=iossoftware&bubble=apple_music) no-repeat center;" class="guide_app_link_btn_apple"></a>
+								<a title="Play Store" target="_blank" class="sejongapp_btn" href='https://play.google.com/store/apps/details?id=kr.ac.sejong.smartcampus&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' class="guide_app_link_btn_google"></a>
+							</div>
+						</div
+						><div class="guide_app_mobile_cont">
+							<img src="static/image/ucheck_guide.png" class="guide_app_mobile wow animated fadeInRight">
+							<div id="guide_app_container" class="guide_app_container">
+								<a title="App Store" target="_blank" href="https://apps.apple.com/us/app/ucheck-plus/id1139582817?mt=8" style="overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2019-08-29&kind=iossoftware&bubble=apple_music) no-repeat center;" class="guide_app_link_btn_apple"></a>
+								<a title="Play Store" target="_blank" class="sejongapp_btn" href='https://play.google.com/store/apps/details?id=com.libeka.attendance.ucheckplusstud'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' class="guide_app_link_btn_google"></a>
+							</div>
 						</div>
 					</div>
 
